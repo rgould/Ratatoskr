c0d51e32dfd66395bfedd297fc3dd91265644e7a
rgould <rwgould@gmail.com>
2011-10-09 18:05:20 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+> Ratatoskr heitir íkorni, er renna skal
+> at aski Yggdrasils, arnar orð
+> hann skal ofan bera ok segja Niðhöggvi niðr.
+
+Ratatoskr is the source code for the littlevikinggames.com website.
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ratatoskr::Application.config.secret_token = '1f276ae5d53a7e50b03d2717ea6d8c1b10831e75eb039664a2515d55df87481857cb775a020215eb85648d155a8bdd39338389420d636ebcd7df456c88168f59'
