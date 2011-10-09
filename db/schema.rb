4d3ec044e2460e5fcc260103a0cfccd056918bdf
rgould <rwgould@gmail.com>
2011-10-09 18:51:35 -0400

    Add unique index on email to subsribers

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111009221027) do
+ActiveRecord::Schema.define(:version => 20111009225040) do
 
   create_table "subscribers", :force => true do |t|
     t.string "email"
   end
+
+  add_index "subscribers", ["email"], :name => "index_subscribers_on_email", :unique => true
 
 end
